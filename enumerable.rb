5874a0ea787c6694cdfe80593c8600e8a1dcc462
Felix Ouma <fatonoh@gmail.com>
2022-08-19 14:43:53 +0300

    Create custom enumerable & fix linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
+
 module MyEnumerable
-    def all?
-      result = true
-  
-      each { |item| result = false unless yield item }
-  
-      result
-    end
-  
-    def any?
-      result = false
-  
-      each { |item| result = true if yield item }
-  
-      result
-    end
-  
-    def filter?
-      result = []
-  
-      each { |item| result.push(item) if yield item }
-      result
-    end
+  def all?
+    result = true
+
+    each { |item| result = false unless yield item }
+
+    result
   end
-  
+
+  def any?
+    result = false
+
+    each { |item| result = true if yield item }
+
+    result
+  end
+
+  def filter
+    result = []
+
+    each { |item| result.push(item) if yield item }
+    result
+  end
+end
---
+++ mylist.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative 'enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&value)
+    @list.each(&value)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+puts(list.filter(&:even?))
